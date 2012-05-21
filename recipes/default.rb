a70c0285d6c2b7876e2d593e63953b9e8ff455e4
Karl Freeman <karlfreeman@gmail.com>
2012-05-21 12:05:17 +0200

    make sure bluepill is being :install'ed

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,9 +17,13 @@
 # limitations under the License.
 #
 
-gem_package "i18n"
+gem_package "i18n" do
+  action :install
+end
+
 gem_package "bluepill" do
   version node["bluepill"]["version"] if node["bluepill"]["version"]
+  action :install
 end
 
 [
