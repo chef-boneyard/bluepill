9fafdabc8a950231065e0976e20eba88015956a3
AJ Christensen <aj@junglist.gen.nz>
2012-08-29 10:26:22 +1200

    [COOK-1592] Add "infections" for bluepill_service resource

---
+++ test/kitchen/cookbooks/bluepill_test/files/default/tests/minitest/helpers.rb
@@ -0,0 +1,9 @@
+module BluepillTestHelpers
+  include MiniTest::Chef::Assertions
+  include MiniTest::Chef::Context
+  include MiniTest::Chef::Resources
+
+  MiniTest::Chef::Resources.register_resource(:bluepill_service)
+  MiniTest::Chef::Infections.infect_resource :bluepill_service, :enabled, :be_enabled
+  MiniTest::Chef::Infections.infect_resource :bluepill_service, :running, :be_running
+end
