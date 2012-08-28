8d23fe06c72b94a30353134adcf738dc49944ba4
AJ Christensen <aj@junglist.gen.nz>
2012-08-29 10:26:22 +1200

    [COOK-1592] Bluepill: Integration tests -- simulate a service
    Host a ruby TCP server, connect to that with netcat supervised by bluepill

--- test/kitchen/cookbooks/bluepill_test/files/default/tests/minitest/default_test.rb
+++ test/kitchen/cookbooks/bluepill_test/files/default/tests/minitest/default_test.rb
@@ -17,7 +17,20 @@ describe_recipe 'bluepill_test::default' do
 
   describe "runs the application as a service" do
     let(:service) { bluepill_service(node['bluepill_test']['service_name']) }
-    it { service.must_be_running }
     it { service.must_be_enabled }
+    it { service.must_be_running }
+  end
+
+  describe "spawn a netcat tcp client repeatedly" do
+    let(:port) { node['bluepill_test']['tcp_server_listen_port'] }
+    let(:secret) { node['bluepill_test']['secret'] }
+    it "should receive a TCP connection from netcat" do
+      TCPServer.open("localhost", port) do |server|
+        client = server.accept
+        client_secret = client.gets.strip!
+        assert_equal secret, client_secret
+        client.close
+      end
+    end
   end
 end
