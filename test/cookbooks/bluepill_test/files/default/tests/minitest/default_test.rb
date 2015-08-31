6792cdd44b6e0a2c64e58e4aea34c163f98a05a2
Tim Smith <tsmith84@gmail.com>
2015-08-31 15:50:48 -0700

    Add name metadata to test + rubocop fixes

--- test/cookbooks/bluepill_test/files/default/tests/minitest/default_test.rb
+++ test/cookbooks/bluepill_test/files/default/tests/minitest/default_test.rb
@@ -1,34 +1,36 @@
-require_relative "./support/helpers"
+require_relative './support/helpers'
 
 describe_recipe 'bluepill_test::default' do
   include BluepillTestHelpers
 
-  describe "create a bluepill configuration file" do
-    let(:config) { file(::File.join(node['bluepill']['conf_dir'],
-                                    node['bluepill_test']['service_name'] +
-                                    ".pill")) }
+  describe 'create a bluepill configuration file' do
+    let(:config) do
+      file(::File.join(node['bluepill']['conf_dir'],
+                       node['bluepill_test']['service_name'] +
+                       '.pill'))
+    end
     it { config.must_exist }
 
-    it "must be valid ruby" do
+    it 'must be valid ruby' do
       assert(shell_out("ruby -c #{config.path}").exitstatus == 0)
     end
   end
 
-  describe "runs the application as a service" do
+  describe 'runs the application as a service' do
     let(:service) { bluepill_service(node['bluepill_test']['service_name']) }
     it { service.must_be_enabled }
     it { service.must_be_running }
   end
 
-  it "the default log file must exist (COOK-1295)" do
+  it 'the default log file must exist (COOK-1295)' do
     file(node['bluepill']['logfile']).must_exist
   end
 
-  describe "spawn a netcat tcp client repeatedly" do
+  describe 'spawn a netcat tcp client repeatedly' do
     let(:port) { node['bluepill_test']['tcp_server_listen_port'] }
     let(:secret) { node['bluepill_test']['secret'] }
-    it "should receive a TCP connection from netcat" do
-      TCPServer.open("localhost", port) do |server|
+    it 'should receive a TCP connection from netcat' do
+      TCPServer.open('localhost', port) do |server|
         client = server.accept
         assert_instance_of TCPSocket, client
 
--- test/cookbooks/bluepill_test/recipes/default.rb
+++ test/cookbooks/bluepill_test/recipes/default.rb
@@ -1,19 +1,19 @@
-include_recipe "bluepill"
+include_recipe 'bluepill'
 
 # Boring
-package "nc" do
+package 'nc' do
   package_name case node['platform_family']
-               when "debian"
-                 "netcat"
+               when 'debian'
+                 'netcat'
                else
-                 "nc"
+                 'nc'
                end
 end
 
 # This fake services uses Netcat to continuously send the secret
 # (attribute) to the tcp_server_listen_port, which we bind in the test
 template ::File.join(node['bluepill']['conf_dir'],
-                     node['bluepill_test']['service_name'] + ".pill")
+                     node['bluepill_test']['service_name'] + '.pill')
 
 bluepill_service node['bluepill_test']['service_name'] do
   action [:enable, :load, :start]
