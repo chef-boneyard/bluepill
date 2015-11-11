ae39764e18830e24d00a8c43dca21fdd1934d001
Tim Smith <tsmith84@gmail.com>
2015-11-10 16:00:39 -0800

    Use rsyslog 2.0 for Chef 11 compat

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '2.4.0'
 recipe 'bluepill::default', 'Installs bluepill rubygem and sets up management directories'
 
-depends 'rsyslog'
+depends 'rsyslog', '~> 2.0'
 
 source_url 'https://github.com/chef-cookbooks/bluepill' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/bluepill/issues' if respond_to?(:issues_url)
