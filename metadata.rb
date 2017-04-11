f4f2142a4d5785773fe597c51202ccda0bda11a0
Tim Smith <tsmith@chef.io>
2017-04-11 16:16:57 -0700

    Add supported platforms
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,13 +1,17 @@
 name 'bluepill'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Installs bluepill gem and configures to manage services, includes bluepill_service LWRP'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '4.0.0'
 recipe 'bluepill::default', 'Installs bluepill rubygem and sets up management directories'
 
 depends 'rsyslog', '>= 2.0'
+
+%w(amazon centos debian opensuse opensuseleap oracle redhat scientific suse ubuntu).each do |os|
+  supports os
+end
 
 source_url 'https://github.com/chef-cookbooks/bluepill'
 issues_url 'https://github.com/chef-cookbooks/bluepill/issues'
